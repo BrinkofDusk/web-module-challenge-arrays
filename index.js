349af8ba7430aab90903e273a0d5e9fe59f9de62
Austin Jansen <ausitnjansen94@gmail.com>
2020-09-09 19:36:20 -0400

    Question 7 complete, cases sensitive function

--- index.js
+++ index.js
@@ -134,11 +134,13 @@ Your function should accept:
 
 and should return a new array that is identical to the old array. You can name the new array however you'd like. */
 
-function copy(/*code here*/){
-
-    /*code here*/
+function copy(arry1, arry2) {
+    // must be new variable made for new array.
+    arry2 = arry1.slice();
+    return arry2
 
 }
+
 
 /* Task 7: July 7th is "World Chocolate Day" and Baskin Robins wants to create promotional materials highlighting all of their chocolate flavors. Write a function that checks every item in the array for a given string and returns a new array called filteredArray with just these values. Rather than hardcoding "chocolate" into your function, pass a string as a parameter, and invoke with the argument "chocolate". This way you could also filter for "Vanilla", "Sherbert", etc. when those holidays roll around.
 
@@ -155,12 +157,16 @@ DO NOT USE ADVANCED ARRAY METHODS (i.e. .filter) to solve this problem.
 
 hint - you can use the .includes method to help you solve this */
 
-function filterByWord(/*code here*/){
-
-    /*code here*/
-
+function filterByWord(arry, flav) {
+    let promoArray;
+    for (i = 0; i < arry.length; i++) {
+        if (arry[i].includes(flav)) {
+            promoArray.push(arry[i]);
+        }
+    }
+    return promoArray;
 }
-
+//Case Sensitive...
 
 
 /* 🧁🍦🍨 STRETCH 🍨🍦🍫*/ 
